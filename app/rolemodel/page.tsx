994fdd1f467ddb19937c7d3f8f2de51c5f0188a8
gloomeejeremee <jeremeesmile@gmail.com>
2026-04-30 00:12:44 +0900

    Added role model page (Jaerim)

---
+++ app/rolemodel/page.tsx
@@ -0,0 +1,8 @@
+export default function RoleModelPage() {
+  return (
+    <div style={{ padding: "40px", maxWidth: "600px", margin: "0 auto" }}>
+      <h1>Find Your Role Model</h1>
+      <p>Tell us about your life goals and we will suggest a role model for you.</p>
+    </div>
+  );
+}
